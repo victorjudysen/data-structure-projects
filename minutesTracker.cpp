b634b8dc66b26673713114f330308899f0bbf35e
victorjudysen <117171668+victorjudysen@users.noreply.github.com>
2024-05-06 23:15:50 +0300

    Update minutesTracker.cpp

--- minutesTracker.cpp
+++ minutesTracker.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 using namespace std;
 
+int calculateValues() {
+
+}
 
 int main() {
     int hours;
-    int minutes;
+    int oldMinutes;
     int seconds;
 
     cout << "Enter the number of hours: ";
     cin >> hours;
     
     cout << "Enter the number of minutes: ";
-    cin >> minutes;
+    cin >> oldMinutes;
     
     cout << "Enter the number of seconds: ";
     cin >> seconds;
+
+
 
 }
